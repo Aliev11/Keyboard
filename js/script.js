7efc4e2ca4edd7961781d0e41f9aa4ebba274a0b
Aliev11 <amraha641@gmail.com>
2022-11-05 13:33:45 +0300

    added new buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-let keyboard = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
-let out = document.querySelector('.textarea');
+let keyboard = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
+let out = document.querySelector('textarea');
 function init() {
     let output = '';
     for (let i = 0; i < keyboard.length; i++) {
         output += '<div class = "k-key" data = "' + keyboard[i] + '">' + String.fromCharCode(keyboard[i]) + '</div>';
-        if (i == 34) {
-            output += '<div class="k-key enter">Enter</div>';
+        if (i == 33) {
+            output += '<div class="k-key ctrl">Ctrl</div>';
         }
-        if (i == 11 || i == 23 || i == 34) {
+        if (i == 11 || i == 23 || i == 33) {
             output += '<div class="clearfix"></div>';
         }
     }
     output += '<div class="k-key backspace">BackSpace</div>';
+    output += '<div class="k-key shift">Shift</div>';
+    output += '<div class="k-key slice"></div>';
+    output += '<div class="k-key enter">Enter</div>';
     document.querySelector('#keyboard').innerHTML = output;
 }
 init();
@@ -21,7 +24,7 @@ function backspace() {
         elem.classList.remove('active');
     });
     back.classList.add('active');
-    let a = document.querySelector('.textarea').textContent;
+    let a = document.querySelector('textarea').textContent;
     let b = a.split('');
     b.pop();
     let c = b.join('')
@@ -59,24 +62,16 @@ document.querySelectorAll('.k-key').forEach(element => {
             }
         }
     
-
-    
-        
-    let enter = document.querySelector('.enter');
-    enter.onclick = function() {
+    let slice = document.querySelector('.slice');
+    slice.onclick = function () {
         document.querySelectorAll('.k-key').forEach(elem => {
             elem.classList.remove('active');
         });
-        enter.classList.add('active');
-        let a = document.querySelector('.textarea').innerHTML;
-        let b = a.split();
-        let Example = document.querySelector('.textarea');
-        let c = '';
-        for(let key in b){
-            c += `${b[key]}<br>1`;
-        }
-        console.log(c);
-        Example.innerHTML = c;
+        slice.classList.add('active');
+        let a = document.querySelector('textarea').innerHTML;
+        a += '&nbsp';
+        document.querySelector('textarea').innerHTML = a;
+
     }
         
     
